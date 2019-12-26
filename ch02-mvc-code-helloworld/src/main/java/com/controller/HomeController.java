a112bbb7c0c55f81d05dab8b0cfc10b2e46e6f3f
cj <417278483@qq.com>
2019-12-26 20:17:30 +0800

    添加了第二章的起始项目模板

---
+++ ch02-mvc-code-helloworld/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author cj
+ * @date 2019/12/26
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ ch02-mvc-code-helloworld/src/main/java/config/AppConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cj
+ * @date 2019/11/25
+ */
+
+@Configuration
+//@ComponentScan({"com.service"})
+public class AppConfig {
+
+
+}
